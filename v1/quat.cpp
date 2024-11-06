60a9c902cab8e2cbfb8bcb974ffc6fa350fab764
Tung Nguyen <tunghnguyen.ac@proton.me>
2024-11-06 15:26:08 -0800

    Added division operator overloading for quaterion type

--- v1/quat.cpp
+++ v1/quat.cpp
@@ -29,6 +29,12 @@ quat_t &quat_t::operator*=(quat_t const &rhs) {
   return (*this);
 }
 
+quat_t &quat_t::operator/=(float32_t rhs) {
+  (*this) = (*this) / rhs;
+
+  return (*this);
+}
+
 quat_t &quat_t::normalize() {
   arm_quaternion_normalize_f32(this->data, this->data, 1);
 
@@ -62,6 +68,16 @@ quat_t operator*(float32_t lhs, quat_t const &rhs) {
 quat_t operator*(quat_t const &lhs, quat_t const &rhs) {
   quat_t res;
   arm_quaternion_product_f32(lhs.data, rhs.data, res.data, 1);
+
+  return res;
+}
+
+quat_t operator/(quat_t const& lhs, float32_t rhs) {
+  quat_t res;
+  res.w = lhs.w / rhs;
+  res.x = lhs.x / rhs;
+  res.y = lhs.y / rhs;
+  res.z = lhs.z / rhs;
 
   return res;
 }
